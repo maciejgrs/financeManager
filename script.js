942fbf48b655f4cb3b37568f602f44ee6ac00ae9
Maciej Grs <maciejgrs@gmail.com>
2021-02-22 18:56:09 +0100

    ability to add trans

--- script.js
+++ script.js
@@ -9,23 +9,73 @@ const incomeArea = document.querySelector(".income-area");
 const availableMoney = document.querySelector(".available-money");
 const expenseArea = document.querySelector(".expenses-area");
 const deleteAll = document.querySelector(".delete-all");
+let id = 0;
+const moneyArray = [];
+const funkcja = () => {
+  checkCategory(categorySelect.value);
 
+  addPanel.style.display = "none";
+
+  buildingItem(categoryIcon, inputName.value, inputAmount.value);
+  if (inputAmount.value > 0) {
+    incomeArea.appendChild(newTrans);
+  } else {
+    expenseArea.appendChild(newTrans);
+  }
+};
 
 addButton.addEventListener("click", () => {
-    addPanel.style.display = "flex";
-  });
-  saveBtn.addEventListener("click", () => {
-    if (
-      categorySelect.value !== "none" &&
-      inputName.value !== "" &&
-      inputAmount.value !== ""
-    ) {
-      funkcja();
-    } else {
-      alert("Musisz wypełnić wszystkie pola");
-    }
-  });
-  
-  cancelBtn.addEventListener("click", () => {
-    addPanel.style.display = "none";
-  });
+  addPanel.style.display = "flex";
+});
+saveBtn.addEventListener("click", () => {
+  if (
+    categorySelect.value !== "none" &&
+    inputName.value !== "" &&
+    inputAmount.value !== ""
+  ) {
+    funkcja();
+  } else {
+    alert("Musisz wypełnić wszystkie pola");
+  }
+});
+
+cancelBtn.addEventListener("click", () => {
+  addPanel.style.display = "none";
+});
+
+const buildingItem = (category, inName, amount) => {
+  newTrans = document.createElement("div");
+  newTransName = document.createElement("p");
+  newAmount = document.createElement("p");
+  newTransName.innerHTML = `${category} ${inName}`;
+  newTrans.setAttribute("id", id);
+  id++;
+  newAmount.innerText = amount;
+  newTrans.classList.add("transaction");
+
+  newTrans.appendChild(newTransName);
+  newTrans.appendChild(newAmount);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("deleting");
+  deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
+  newTrans.appendChild(deleteBtn);
+
+  moneyArray.push(parseFloat(amount));
+};
+const checkCategory = (transaction) => {
+  switch (transaction) {
+    case "income":
+      categoryIcon = '<i class="fas fa-money-bill-wave"></i> ';
+      break;
+    case "shopping":
+      categoryIcon = '<i class="fas fa-cart-arrow-down"></i>';
+      break;
+    case "food":
+      categoryIcon = '<i class="fas fa-hamburger"></i>';
+      break;
+    case "cinema":
+      categoryIcon = '<i class="fas fa-film"></i>';
+      break;
+  }
+};
